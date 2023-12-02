9241738672222385302f3fd40317408fdd5962ee
Yağız Aydın <yagiz.aydin@btctrader.com>
2023-12-02 11:24:50 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import { create } from 'zustand'
 import CodePreview from './components/CodePreview'
 import Details from './components/Details'
 import Scene from './components/Scene'
+import { mountStoreDevtool } from 'simple-zustand-devtools';
 
 const useStore = create((set) => ({
   count: 1,
   inc: () => set((state) => ({ count: state.count + 1 })),
 }))
+
+
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'production' ) {
+  mountStoreDevtool('COUNT_STORE', useStore);
+}
 
 function Counter() {
   const { count, inc } = useStore()
